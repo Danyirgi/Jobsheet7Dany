6ee5d9b59bb8f5ff400a4331526fa833df5d4b84
Amel <Amel@lenovo>
2018-08-03 12:53:29 +0700

    Tambah Input Bahan 1.

--- src/Jobsheet7dany/SaranBahan2Resep.java
+++ src/Jobsheet7dany/SaranBahan2Resep.java
@@ -1,12 +1,23 @@
 
 package Jobsheet7dany;
 
+import java.util.Scanner;
+
 
 public class SaranBahan2Resep {
     public static void main(String[] args) {
     String identitas = "Dany Achmad Virgian / XRPL 5 / 10";
     System.out.println("Identitas : "+ identitas);
     System.out.print("\nSaran Resep dari Bahan Milik Anda\n");  
+    
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Bahan Pertama : ");
+    System.out.println("1. Pisang ");
+    System.out.println("2. Telur ");
+    System.out.print("Masukkan no pilihan Anda : ");
+    int bahan1 = scanner.nextInt();
+    
+    
     
     
     }
