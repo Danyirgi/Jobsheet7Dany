ab924c7a2c50778cfa2ccfd9eb48b4338e7b1b73
Amel <Amel@lenovo>
2018-08-03 11:07:29 +0700

    Initial commit

--- src/Jobsheet7dany/CobaPercabangan.java
+++ src/Jobsheet7dany/CobaPercabangan.java
@@ -1,5 +1,7 @@
 package Jobsheet7dany;
 
+import java.util.Scanner;
+
 
 public class CobaPercabangan {
 
@@ -7,7 +9,23 @@ public class CobaPercabangan {
     public static void main(String[] args) {
        String identitas ="Dany Achmad Virgian/ XRPL 5 / 10";
        System.out.println("identitas:"+ identitas);
-     
+       Scanner scanner = new Scanner(System.in);
+       System.out.print("nilai a ?");
+       int a = scanner.nextInt();
+       System.out.println("\nIF.. 1");
+       if(a < 5) System.out.println("nilai a kurang dari 5");
+       System.out.println("\nIF.. 2");
+       if(a == 5) System.out.println("nilai a sama dari 5");
+       System.out.println("\nIF.. 3");
+       if(a > 5){
+           System.out.println("isi variabel a: "+a);
+           System.out.println("NILAI A LEBH DARI 5");
+           
+           System.out.println("\nIF..ELSE..");
+           if(a < 5) {
+               System.out.println("isi variabel  a : " + a);
+               System.out.println("nilai a kurang dari a");
+           } else System.out.println("nilai a lebih dari atau sama dengan 5");
            
            
            
@@ -15,6 +33,6 @@ public class CobaPercabangan {
            
        }
        
-    
+    }
     
 
