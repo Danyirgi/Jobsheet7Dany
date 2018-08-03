a469d1e51a51c15220ad1da4042731e0d219eabc
Amel <Amel@lenovo>
2018-08-03 12:48:37 +0700

    Initial Commit

---
+++ src/Jobsheet7dany/SaranBahan2Resep.java
@@ -0,0 +1,18 @@
+
+package Jobsheet7dany;
+
+
+public class SaranBahan2Resep {
+    public static void main(String[] args) {
+    String identitas = "Dany Achmad Virgian / XRPL 5 / 10";
+    System.out.println("Identitas : "+ identitas);
+    System.out.print("\nSaran Resep dari Bahan Milik Anda\n");  
+    
+    
+    }
+}
+    
+    
+    
+    
+
