318132884d000d945641bc593a894b43b0d3ed24
Amel <Amel@lenovo>
2018-08-03 11:28:35 +0700

    Initial commit

--- src/Jobsheet7dany/CobaPercabangan.java
+++ src/Jobsheet7dany/CobaPercabangan.java
@@ -44,14 +44,24 @@ public class CobaPercabangan {
                   System.out.println("nilai a sama dari 6");
            }else
                   System.out.println("nilai a lebih dari 6");
-           
-           
+           System.out.println("\nNested IF");
+           if(a < 7){
+               System.out.println("nilai a kurang dari 7");
+               if(a > 2)
+                    System.out.println("nilai a lebih dari 2");
+               if(a < 4)
+            System.out.println("nilai a kurang dari 4");
            }
-           
-           
-           }
-           
        }
+    }
+    }
+           
+           
+           
+           
+           
+           
+       
        
     
     
